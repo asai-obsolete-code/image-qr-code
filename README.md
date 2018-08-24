9db4d5a21501b511bb6cd1c13bc36c8c7d5afd53
Masataro Asai <guicho2.71828@gmail.com>
2018-08-24 09:23:52 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+Generate a color QR code based on pictures!
+
+[out.png](out.png)
+
+[lenna.png](lenna.png)
+
---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy
+
+def main(text,picture,output):
+    import qrcode
+    qr = qrcode.make(text)
+    qr = numpy.asarray(qr).astype(float)
+    qr.flags.writeable = True
+    qr = numpy.expand_dims(qr,3)
+    print(type(qr))
+    print(qr.shape)
+    print(qr.min(),qr.max())
+    qr2 = qr / 3
+    qr3 = 1- qr
+
+    import imageio
+    im = imageio.imread(picture)
+    print(im.shape)
+    
+    from skimage.color import rgb2gray
+    # im = rgb2gray(im)
+    print(im.shape)
+    
+    print(im.min(),im.max())
+    # im = (im / 2)
+    # print(im.min(),im.max())
+
+    from skimage.transform import resize
+    im = resize(im, tuple(qr.shape)+(3,), preserve_range=True)
+    print(im.shape)
+
+    imageio.imwrite("tmp.png", im)
+
+    result = ((1-qr) * im) + qr*255
+    print(result.shape)
+    imageio.imwrite(output, result)
+
+import sys
+main(*sys.argv[1:])
+
+
+main("hellooooooooooooooooooooooooo","lenna.png","out.png")
